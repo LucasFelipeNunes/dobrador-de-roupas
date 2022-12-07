c536a42813707d1a6bd2bf43807b85c9a882c586
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-12-06 22:07:30 -0300

    Adicionando código dobrador de roupas

--- dobrador_de_roupas.cpp
+++ dobrador_de_roupas.cpp
@@ -1,9 +1,37 @@
+#include <Servo.h>
+Servo motor1, motor2, motor3;
+int botao = 12;
+bool estadoBotao = false;
 void setup() {
-  // put your setup code here, to run once:
-
+	motor1.attach[3];
+	motor2.attach[4];
+	motor3.attach[5];
+	pinMode(botao, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+	estadoBotao = digitalRead(botao);
+	if(estadoBotao == HIGH){
+		motor1.write(175);
+		delay(1000);
+		motor1.write(0);
+		delay(1000);
+		motor2.write(175);
+		delay(1000);
+		motor2.write(0);
+		delay(1000);
+		motor1.write(175);
+		delay(1000);
+		motor1.write(0);
+		delay(1000);
+		motor3.write(175);
+		delay(1000);
+		motor3.write(0);
+		delay(1000);
+	}
+	else{
+		motor1.write(0);
+		motor2.write(0);
+		motor3.write(0);
+	}
 }
